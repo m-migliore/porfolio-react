0402231e7d3d88f55e6793fce8d6303628074aa6
Matt Migliore <mmigliore311@gmail.com>
2019-02-28 12:09:55 -0500

    add final touches to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,8 +16,13 @@ class Contact extends Component {
       <div id="contact" className="content-box jello animated">
         <CloseButton name="Contact" />
         <h2>Contact</h2>
+        <h3>Thank you for visiting my site!</h3>
         <p>Please feel free to contact me through any of the options below</p>
-        <p><h4>Email</h4> <a href="mailto:migliorewd@gmail.com">migliorewd@gmail.com</a></p>
+        <div className="contact-icons">
+          <a href="https://www.linkedin.com/in/matthew-migliore-b231a060/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+          <a href="https://github.com/m-migliore" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+          <a href="mailto:migliorewd@gmail.com"><i class="far fa-envelope"></i></a>
+        </div>
 
       </div>
     );
